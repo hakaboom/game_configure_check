f50a72d1fb0a16dae3c8b8ed4033d536badb6196
huangziqing <270704881@qq.com>
2021-07-05 18:40:11 +0800

    update

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 import os
 
 
-WORK_PATH = os.path.realpath('table')
-REPORT_NAME = '检查结果.xlsx'
-REPORT_PATH = os.path.realpath('table')
-CHECKLIST_NAME = 'checkList.xls'
+WORK_PATH = os.path.realpath('table')  # 工作路径
+REPORT_NAME = '检查结果.xlsx'  # 返回检查结果表格的文件名
+REPORT_PATH = os.path.realpath('table')  # 返回检查结果表格的路径
+CHECKLIST_NAME = 'checkList.xls'  # 读取检查规则的表格
